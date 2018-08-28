a61d5b2db1842802c061a6577bd0d5a3537c4048
Mohamed Samir <gm.mohamedsamir@gmail.com>
2018-08-28 02:11:46 +0200

    some updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -60,7 +60,7 @@
         <!-- Compiled and minified JavaScript -->
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
-        <script src="{{ asset('js/toastr.min.js') }}"></script>
+        <script src="{{ asset('js/toastr.min.js', true) }}"></script>
         <script type="text/javascript">
             @if(session()->has('success'))
             toastr.success("{{ session()->get('success') }}");
